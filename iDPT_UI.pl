e412c7acb628b5c07f0468d9800e673f4c52669d
HackerOrientado <hackerorientado@protonmail.com>
2017-03-08 02:17:24 -0300

    iDPT v1.1 - Color, English, better code

--- iDPT_UI.pl
+++ iDPT_UI.pl
@@ -1,54 +1,68 @@
 #!/usr/bin/perl
+#
+# This tool is under license GPL v3
+# This tool is made in perl and HackerOrientado made this tool
+# HackerOrientado ( Ciência Hacker )
+#
 
+#################### Modules
 use strict;
 use warnings;
 use Time::localtime;
 use Tkx;
 use File::Copy;
 use utf8;
+########################
 
+#################### Input/Output UTF-8
 binmode(STDIN, ":utf8");
 binmode(STDOUT, ":utf8");
+########################
 
+#################### System commands
+my $version = "1.1";
 system "cls";
-system "title iDPT - v1.0";
+system "title iDPT - v$version";
 
-# VARIAVEIS GLOBAIS
+#################### Global variables
 my ( $count, $numArquivos, $box_ip_Local_iDevice, $ip_Local_iDevice, $Janela_Principal, $label, $b_BackupPhotos, $b_Ajuda, $b_Fechar );
 my @arquivos;
 my @arquivosCheck;
 my @infos_iDevice;
 my @push_array_info;
+########################
 
-# Data para a criação da pasta
+#################### Variables of date
 my $dia = localtime->mday();
 my $mes = localtime->mon() +1;
 my $ano = localtime->year() + 1900;
 my $pasta = "$dia-$mes-$ano";
 #############
 
-# sub rotinas
+#################### Sub routine of help popup
 sub mensagem_ajuda {
 
 	Tkx::tk___messageBox (
 		
 		-parent		=> $Janela_Principal,
 		-icon 		=> "info",
-		-title 		=> "Ajuda",
+		-title 		=> "Help",
 		-type 		=> "ok",
-		-message	=> "Menu de ajuda para o iDPT\n\n" .
-		"Para usar o programa, clique na opção desejada\n" .
-		"e digite a senha 'root' do seu OpenSSH ( questões de seg. )\n\n",
+		-message	=> "Help menu for iDPT\n\n" .
+		"Click in the option and enter your\n" .
+		"OpenSSH root password\n\n",
 		
 	);
 		
 }
+########################
 
+#################### Sub routine of Backup Photos
 sub ssh() {
 
 	system "mkdir backup$pasta";
 
-	print "Digite a senha SSH do usuario root de seu iDevice\n\n";
+	print "Enter with your SSH root password\n\n";
 	
 	system "scp root\@$ip_Local_iDevice:/var/mobile/Media/DCIM/100APPLE/* .";
 	
@@ -73,66 +87,91 @@ sub ssh() {
 	print "Backup realizado com sucesso\!\n\n";
 	
 }
+########################
 
-# Janela Principal
+#################### Main window configure
 $Janela_Principal = Tkx::widget -> new ( "." );
-$Janela_Principal -> g_wm_title ( "iDPT - v1.0" );
-$Janela_Principal -> g_wm_minsize ( 300, 200 );
+$Janela_Principal -> g_wm_title ( "iDPT - v$version" );
+$Janela_Principal -> m_configure ( -background => "#1fa679" );
+$Janela_Principal -> g_wm_minsize ( 300, 330  );
+########################
 
-#################### Interface
+
+#################### Label interface
 $label = $Janela_Principal -> new_ttk__label ( -text => "
-               iDPT - iDevice Perl Tool - v1.0\n
+iDPT - iDevice Perl Tool - v$version
 
-          Entre com o IP Local de seu iDevice e
- clique no botão desejado para realizar sua ação
+Made by HackerOrientado ( Ciência Hacker )
+
+Enter with your local IP of your iDevice
+Click on the button for make your action
 " );
 
-$box_ip_Local_iDevice = $Janela_Principal -> new_ttk__entry ( -textvariable => \$ip_Local_iDevice );
-$box_ip_Local_iDevice -> insert (0, "IP local do iDevice" );
+$label -> m_configure ( -justify	=> "center" );
+$label -> m_configure ( -background => "#1fa679" );
 
-#################### Botões 
+$box_ip_Local_iDevice = $Janela_Principal -> new_ttk__entry ( -textvariable => \$ip_Local_iDevice );
+$box_ip_Local_iDevice -> m_configure ( -foreground 	=> "black" );
+$box_ip_Local_iDevice -> m_configure ( -justify	=> "center" );
+$box_ip_Local_iDevice -> insert (0, "Your iDevice local IP" );
+########################
+
+#################### Buttons 
 $b_BackupPhotos = $Janela_Principal -> new_button (
 
-	-text => "Conect ao SSH",
-	-command => sub {
+	-background	=> "white",
+	-text 		=> "Backup photos",
+	-command 	=> sub {
 	
 		$b_BackupPhotos -> m_configure (
-		
-			-text => "Conectando ..",
-			
-		);
-		
-		Tkx::after ( 
-			500, sub { 
-				$b_BackupPhotos -> m_configure ( 
-			
-					-text => "Conect ao SSH", 
 
-				) 
-			} 
+			-text 		=> "Connecting.....",
+		
 		);
 		
-		return ssh();
+		Tkx::after (
+		
+			500, sub {
+			
+				$b_BackupPhotos -> m_configure ( 
+
+					-text 		=> "Conect ao SSH", 
+					
+				) 
+			}
+		);
+		
+		Tkx::after (
+		
+			500, sub {
+			
+				return ssh()
+			
+			}
+		
+		);
+		
 		
 	},
-
 );
 
 $b_Ajuda = $Janela_Principal -> new_button (
 
-	-text => "Ajuda",
-	-command => \&mensagem_ajuda,
+	-background	=> "white",
+	-text 		=> "Help",
+	-command 	=> \&mensagem_ajuda,
 
 );
 
 $b_Fechar = $Janela_Principal -> new_button (
 
-	-text => "Fechar programa",
-    -command => sub {
+	-background	=> "white",
+	-text 		=> "Close program",
+    -command 	=> sub {
 		
 		$b_Fechar -> m_configure (
 		
-			-text => "Fechando programa...",
+			-text => "Closing..",
         
 		);
 
@@ -143,11 +182,11 @@ $b_Fechar = $Janela_Principal -> new_button (
 ########################
 
 
-#################### Pack do botões
+#################### Pack buttons
 $label -> g_pack (
 
 	-padx => 0,
-	-pady => 0,
+	-pady => 5,
 
 );
 
@@ -174,14 +213,17 @@ $b_Ajuda -> g_pack (
 
 $b_Fechar -> g_pack (
 
-	-padx => 0,
+	-padx => 10,
 	-pady => 5,
 
 );
 ########################
 
-#Loop para manter a tela
+#################### Loop for main window
 Tkx::MainLoop();
+########################
 
-###########
+
+#################### This space is reserved for futures updates
 # Adicionar uma tela própria para ajuda usando o widget do Tkx e criar novos botões criando PopUP explicando cada menu da ajuda
+# Organizar o code
